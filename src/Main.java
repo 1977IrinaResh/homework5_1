9ef1b5f20029547856f4b17068782e39f66ede7f
irina <irina_resh@mail.ru>
2024-03-12 11:06:20 +0500

    Homework code

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,82 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner console=new Scanner(System.in);
+        System.out.println("Укажите тип установленной операционной системы");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int osType=console.nextInt();
+        int phoneYear;
+        System.out.println("Укажите год выпуска телефона");
+        phoneYear=console.nextInt();
+        if (osType == 0 && phoneYear<2015)  {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        } else if (osType==1 && phoneYear<2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+
+        } else if (osType == 0 && phoneYear>=2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+
+        } else if (osType == 0 && phoneYear>=2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+
+        }
+        System.out.println("Введите год. Значение должно быть больше 1584");
+        int year=console.nextInt();
+        boolean b = year % 4 == 0;
+        boolean c=year%100!=0;
+        boolean d=year%400==0;
+        boolean b1 = (b && d) || c;
+        if (year>1584 && b1) {
+            System.out.println("Год является высокосным");
+        } else if (year<=1584) {
+            System.out.println("Год должен быть больше 1584");
+        }else {
+            System.out.println("Год не является высокосным");
+        }
+
+
+        double deliveryDistance= 95.0;
+        int deliveryTime=1;
+        System.out.println("Введите расстояние до клиента. По умолчанию рассчет будет дан для значения расстояния равного "+deliveryDistance);
+        deliveryDistance=console.nextFloat();
+        if (deliveryDistance<20){
+            System.out.println("Потребуется дней:"+deliveryTime);
+        } else if (deliveryDistance>=20 && deliveryDistance<60) {
+            deliveryTime=deliveryTime++;
+            System.out.println("Потребуется дней:"+deliveryTime);
+        } else if (deliveryDistance>=60 && deliveryDistance<100) {
+            deliveryTime=deliveryTime+2;
+            System.out.println("Потребуется дней:"+deliveryTime);
+
+
+        }else
+            System.out.println("Доставки нет");
+        int monthNumber=12;
+        System.out.println("Введите номер месяца. По умолчанию будет использоватьдя Декабрь");
+        monthNumber=console.nextInt();
+        switch (monthNumber) {
+            case 12,1,2:
+                System.out.println("Время года - зима");
+                break;
+            case 3,4,5:
+                System.out.println("Время года - весна");
+                break;
+            case 6,7,8:
+                System.out.println("Время года - лето");
+                break;
+            case 9,10,11:
+                System.out.println("Время года - осень");
+                break;
+
+            default:
+                System.out.println("Введен некооректный месяц");
+
         }
     }
+
 }
+
+
